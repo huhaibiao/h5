8cb8298d1b916c370c9c0e4bbd30755f717d0f8c
huhaibiao <2451506989@qq.com>
2023-05-14 11:41:51 +0800

    feat(部署)

--- hello.js
+++
@@ -1,5 +0,0 @@
-/*
- * @Author: huhaibiao
- * @Date: 2023-05-14 10:49:38
- */
-console.log('hello')
--- hello2.js
+++
@@ -1,5 +0,0 @@
-/*
- * @Author: huhaibiao
- * @Date: 2023-05-14 11:38:29
- */
-console.log('hello2')
